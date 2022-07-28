e59c2be295f5c4c6d18c7c12abe4da6f105362f9
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2022-07-28 18:09:30 -0300

    codigo inicial wpf em vb net

---
+++ PedidosWPF/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Public Class MainWindow 
+
+    Public Property PaginaAtual As EnPaginaAtual
+
+    Public Sub New()
+        InitializeComponent()
+        PaginaAtual = EnPaginaAtual.Home_Index
+        Console.WriteLine(PaginaAtual)
+    End Sub
+
+End Class
